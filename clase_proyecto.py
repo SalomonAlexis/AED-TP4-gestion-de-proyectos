19cfb4239c7c4ecd92d59693ba47a3a38bdc7ad3
Alexis Salomón <edgar.alexis.salomon@gmail.com>
2022-09-25 23:45:15 -0300

    jajajajaj asjdajkdkjasdhka

--- clase_proyecto.py
+++ clase_proyecto.py
@@ -1,10 +1,6 @@
 __author__ = 'Alexis Salomón'
 #toda en mi
 #megustalacrema
-<<<<<<< HEAD
-#Vamoñubel
-=======
 
-# Probando ano con waska
->>>>>>> cb073a536f57d2b1d1e242a85e5e853737ef3e8a
+
 #lapuedopudrir?
