188a3eb4b174e7d33a98ca4ae63a3083de5d8e98
lauti <lautaro.gonzalez0101@gmail.com>
2022-09-28 16:59:58 -0300

    lala

--- clase_proyecto.py
+++ clase_proyecto.py
@@ -9,7 +9,7 @@ class Proyecto:
         self.tags = tags
         self.url = url
 
-    def __str__(self):
+    def __string__(self):
         mensaje = ""
         mensaje = f'Usuario: {self.nombre_usuario} '
         mensaje += f'Repositorio: {self.repositorio} '
@@ -20,6 +20,14 @@ class Proyecto:
         mensaje += f'URL: {self.url}'
 
         return mensaje
+
+
+def to_str(stringing):
+    cadena = f'| {string.nombre_propietario:<30} | {string.nombre_mascota:<30} | ' \
+             f'| {string.especie:<5} | {string.tipo_atencion:<5} | ${string.costo:<10.2f} |\n ' \
+             f'{"-" * 95:<95}'
+    return cadena
+
 
 class Fecha:
     def __init__(self, dia, mes, año):
--- funciones.py
+++ funciones.py
@@ -1,3 +1,6 @@
+import os.path
+
+
 def menu():
     print('Menu de Opciones')
     print('1 - Cargar proyectos.')
@@ -9,3 +12,47 @@ def menu():
     print('7 - Mostrar archivo.')
     print('8 - Salir.')
     return int(input('Ingrese su opcion: '))
+
+
+def agregar_ordenado(v, registro):
+    n = len(v)
+    izq = 0
+    der = n - 1
+    pos = 0
+    while izq <= der:
+        c = (izq + der) // 2
+        if v[c].repositorio == registro.repositorio:
+            pos = c
+            break
+        if registro.repositorio < v[c].repositorio:
+            der = c + 1
+        else:
+            izq = c + 1
+
+    if izq > der:
+        pos = izq
+
+    v[pos:pos] = [registro]
+
+
+def cargar_vector():
+    vec = []
+    if not os.path.exists('proyectos.csv'):
+        print("No existe el archivo.")
+        return
+
+    m = open("proyectos.csv", mode="rt", encoding="utf8")
+    num_linea = 0
+    for linea in m:
+        if num_linea > 0:
+            lineas = str_linea(linea[:-1])
+            agregar_ordenado(vec, lineas)
+        num_linea += 1
+        m.close()
+        return vec
+
+
+def mostrar_vector(vec):
+    n = len(vec)
+    for i in range(n):
+        print(vec[i])
