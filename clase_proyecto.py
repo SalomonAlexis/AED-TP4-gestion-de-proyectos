3baa133b515a869612070ec404c0de90cd539240
Alexis Salomón <edgar.alexis.salomon@gmail.com>
2022-09-27 23:36:22 -0300

    definí algunas clases

--- clase_proyecto.py
+++ clase_proyecto.py
@@ -1,1 +1,33 @@
 __author__ = 'Alexis Salomón'
+
+
+class Proyecto:
+    def __init__(self, nombre_usuario, repositorio, descripcion, fecha_actualizacion, lenguaje, likes, tags, url):
+        self.nombre_usuario = nombre_usuario
+        self.repositorio = repositorio
+        self.descripcion = descripcion
+        self.fecha_actualizacion = fecha_actualizacion
+        self.lenguaje = lenguaje
+        self.likes = likes
+        self.tags = tags
+        self.url = url
+
+
+class Fecha:
+    def __init__(self, dia, mes, año):
+        self.dia = dia
+        self.mes = mes
+        self.año = año
+
+    def __str__(self):
+        mensaje = f"{self.año}-{self.mes}-{self.dia}"
+        return mensaje
+
+    def año(self):
+        return self.año
+
+    def mes(self):
+        return self.mes
+
+    def dia(self):
+        return self.dia
