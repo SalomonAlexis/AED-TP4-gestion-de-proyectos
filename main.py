0727797d9870e20688345ac1fbfa92610b566c42
Camiun804 <114577221+Camiun804@users.noreply.github.com>
2022-10-01 10:18:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,96 @@
 from funciones import *
 
 
-def test():
-    print(strftime("%y"))
-
-
 def main():
     meses = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
     vec = []
+    opcionUno = False
+    bandera = True
+    opcionCuatro = False
+    opcionSeis = False
 
-    while True:
+    while bandera:
         opcion = menu()
         if opcion == 1:
             vec, omitidos = cargar_vector()
             cargados = len(vec)
-            print(f'Se cargaron {cargados} registros y se omitieron {omitidos}.')
+            enter = input(f'Se cargaron {cargados} registros y se omitieron {omitidos}.')
+            opcionUno = True
 
         elif opcion == 2:
-            tag = input('Ingrese el tag a buscar: ')
-            vector_2 = buscar_tag(vec, tag)
+            if opcionUno:
+                tag = input('Ingrese el tag a buscar: ')
+                vector_2 = buscar_tag(vec, tag)
 
-            almacenar = validar_opcion("¿Desea almacenar en un nuevo archivo?: ")
-            if almacenar.lower() == "si":
-                crear_archivo(tag, vector_2)
-                print("Archivo guardado.")
+                almacenar = validar_opcion("¿Desea almacenar en un nuevo archivo?[si/no]: \n")
+                if almacenar.lower() == "si":
+                    crear_archivo(tag, vector_2)
+                    enter = input("Archivo guardado. Precione enter pata continuar")
+            else:
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
 
         elif opcion == 3:
-            vector_lenguajes, vector_contador = buscar_lenguaje(vec)
-            imprimir_lenguajes(vector_lenguajes, vector_contador)  # poner mensaje para un solo proyecto
-            
-        elif opcion == 4:
-            matriz = generar_matriz(vec)
-            mostrar_matriz(matriz)  # que imprima bien
-
-            mes = int(input('Ingrese el mes [1-12]: ')) - 1
-            suma = sumar_vector(matriz[mes])
-            print(f'\nEn el mes de {meses[mes]} se actualizaron {suma} proyectos.')
-
-        elif opcion == 5:
-            rep = input('Ingrese un repositorio: ')
-            indice = buscar_por_repositorio(rep, vec)
-
-            if indice is None:
-                print('Proyecto no encontrado.')
+            if opcionUno:
+                vector_lenguajes, vector_contador = buscar_lenguaje(vec)
+                imprimir_lenguajes(vector_lenguajes, vector_contador)  
             else:
-                print(vec[indice])
-                editar = validar_opcion('¿Desea editar la URL? [si/no]: ')
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
+                
+        elif opcion == 4:
+            if opcionUno:
+                matriz = generar_matriz(vec)
+                mostrar_matriz(matriz)  
+                mes = int(input('Ingrese el mes [1-12]: ')) - 1
+                suma = sumar_vector(matriz[mes])
+                opcionCuatro = True
+                enter = input(f'\nEn el mes de {meses[mes]} se actualizaron {suma} proyectos. \nPrecione enter para continuar ')
+            else:
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
+                
+        elif opcion == 5:
+            if opcionUno:
+                rep = input('Ingrese un repositorio: ')
+                indice = buscar_por_repositorio(rep, vec)
 
-                if editar == 'si':
-                    editar_url(vec[indice])
-                    print(f'URL actualizado con éxito. Fecha de actualización {vec[indice].fecha_actualizacion}.')
+                if indice is None:
+                    enter = input('Proyecto no encontrado. Precione enter para continuar')
+                else:
+                    print(vec[indice])
+                    editar = validar_opcion('¿Desea editar la URL? [si/no]: \n')
+
+                    if editar == 'si':
+                        editar_url(vec[indice])
+                        enter = input(f'URL actualizado con éxito. Fecha de actualización {vec[indice].fecha_actualizacion}. \n Precione enter para continuar')
+            else:
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
 
         elif opcion == 6:
-            vector_6 = crear_registros(matriz)
-            crear_archivo_bin(vector_6)
+            if opcionUno:
+                if opcionCuatro:
+                    vector_6 = crear_registros(matriz)
+                    crear_archivo_bin(vector_6)
+                    opcionSeis = True
+                    enter = input('Datos guardados. Precione enter para continuar')
+                else:
+                     enter = input('Primero ejecute la opcio Cuatro. Precione enter para continuar')
+            else:
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
 
         elif opcion == 7:
-            vector_7 = generar_vector()
-            matriz_2 = generar_matriz_2(vector_7)
-            mostrar_matriz(matriz_2)
+            if opcionUno:
+                if opcionSeis:
+                    vector_7 = generar_vector()
+                    matriz_2 = generar_matriz_2(vector_7)
+                    mostrar_matriz(matriz_2)
+                    enter = input('Visualizacion finalizada. Precione enter para continuar')
+                else:
+                    enter = input('Primero ejecute la opcio Seis. Precione enter para continuar')
+            else:
+                enter = input('Primero ejecute la opcio uno. Precione enter para continuar')
 
         elif opcion == 8:
-            break
+            bandera = False
+            print('Finalizacion del programa')
         
         else:
             print("Error. Ingrese una opción válida.")
